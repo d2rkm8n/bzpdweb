a0045c326a09492dfe3477cab5d148961ef8c2f9
Vladislav <id.v1rd@gmail.com>
2024-01-24 22:43:03 +0300

    нарисовал что то

--- bzpdweb/settings.py
+++ bzpdweb/settings.py
@@ -129,5 +129,5 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = reverse_lazy('polls:polls')
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
